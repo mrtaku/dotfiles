b2eaa74439ed9e35664e39f01edc6835679531db
mrtaku <moe.ramault@gmail.com>
2024-01-31 00:16:18 +0100

    my dotfiles

---
+++ qtile/config.py
@@ -0,0 +1,217 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, winify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+from libqtile import hook, widget
+from libqtile import bar, layout
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+win = "mod4"
+alt = "mod1"
+
+terminal = guess_terminal()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([win], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([win], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([win], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([win], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([alt], "Tab", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([win, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([win, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([win, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([win, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([win, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([win, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([win, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([win, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([win], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [win, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([win], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([win], "r", lazy.spawn("rofi -show drun"), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([win], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([win], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key(
+        [win],
+        "f",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key([win], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([win, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([win, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    # Key([win], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+]
+
+# groups = [Group(i) for i in "1123456"]
+groups = []
+group_count = 6
+group_layout = "columns"
+
+for i in range(group_count):
+    groups.append(Group(
+        name=str(i + 1),
+        layout= group_layout,
+        label= ""
+    ))
+    keys.extend([
+        Key([win], groups[i].name, lazy.group[groups[i].name].toscreen()),  # Switch to group
+        Key([win, "shift"], groups[i].name, lazy.window.togroup(groups[i].name, switch_group=False)),
+    ])
+
+
+gap_size = 1
+panel_size = 24
+
+borders = {
+    "margin": gap_size,
+    "single_margin": gap_size,
+    "border_width": 2,
+    "border_on_single": True,
+    "border_focus": "#00b4d8",
+    "border_normal": "#323232"
+}
+
+layouts = [
+    layout.Columns(**borders),
+    layout.Max(**borders),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(**borders),
+    # layout.MonadWide(**borders),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="sans",
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayoutIcon(scale=0.75),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([win], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([win], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([win], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
